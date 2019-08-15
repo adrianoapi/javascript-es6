13669d65f4d403030f699e01b6b07342043f1cb4
adrianoapi <adrianoapi@hotmail.com>
2019-08-15 11:59:17 -0300

    add filtro

---
+++ js/filtro.js
@@ -0,0 +1,29 @@
+
+var campoFiltro = document.querySelector("#filtrar-tabela");
+
+campoFiltro.addEventListener("input", function(){
+
+  var pacientes = document.querySelectorAll(".paciente");
+
+  if(this.value.length > 0){
+    for(var i = 0; i < pacientes.length; i++){
+
+      var paciente = pacientes[i];
+      var nome     = paciente.querySelector(".info-nome").textContent;
+
+      if(nome != this.value){
+        paciente.classList.add("hide");
+      }else{
+        paciente.classList.remove("hide");
+      }
+
+    }
+  }else{
+    for(var i = 0; i < pacientes.length; i++){
+      pacientes[i].classList.remove("hide");
+    }
+  }
+
+
+
+});
