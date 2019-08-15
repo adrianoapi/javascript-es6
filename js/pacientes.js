963f1609d3513b0dc14357585b606516a596a4c6
adrianoapi <adrianoapi@hotmail.com>
2019-08-15 17:00:44 -0300

    update style validation ajax

--- js/pacientes.js
+++ js/pacientes.js
@@ -6,14 +6,23 @@ botaoAdicionar.addEventListener("click", function() {
 
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     xhr.addEventListener("load", function() {
-      var response = xhr.responseText;
-      //console.log(typeof xhr.responseText);
-      var pacientes = JSON.parse(response);
 
-      pacientes.forEach(function(paciente){
-        adicionaPaciente(paciente);
-      });
+      var errorAjax = document.querySelector("#erro-ajax");
 
+      if(xhr.status == 200){
+        errorAjax.classList.add("hide");
+        var response = xhr.responseText;
+        //console.log(typeof xhr.responseText);
+        var pacientes = JSON.parse(response);
+
+        pacientes.forEach(function(paciente){
+          adicionaPaciente(paciente);
+        });
+      }else{
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+        errorAjax.classList.remove("hide");
+      }
     });
     xhr.send();
 });
